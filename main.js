39e0274e155bcb6ba8ec0f32fac8c3d66ee152a3
Larissa-LM <larissa.maria@creditas.com>
2022-10-03 15:46:23 -0300

    Trabalhando com while

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-document.querySelector('.tecla_pom');
-document.querySelector('.tecla_clap');
-document.querySelector('.tecla_tim');
-document.querySelector('.tecla_puff');
-document.querySelector('.tecla_splash');
-document.querySelector('.tecla_toim');
-document.querySelector('.tecla_psh');
-document.querySelector('.tecla_tic');
-document.querySelector('.tecla_tom');
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
 
-document.querySelector('#som_tecla_pom').play();
-document.querySelector('#som_tecla_clap').play();
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let cont = 0;
+
+while (cont <= listaDeTeclas.length) {
+    listaDeTeclas[cont].onclick = tocaSomPom;
+    cont += 1;
+} 
